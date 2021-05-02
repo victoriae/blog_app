d4bf3c1b76f2b21eb1f1b4ba66a25848817ae50e
Victoria Becerra <victoria.becerra@merlin.com>
2021-05-02 14:38:09 -0400

    feature: add bootstrap styles to categories form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
+import Nabvar from "bootstrap"
 
 Rails.start()
 Turbolinks.start()
