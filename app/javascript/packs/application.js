9311f56b5c7ee15ba38f42faa5948f26788122d0
Victoria Becerra <victoria.becerra@merlin.com>
2021-05-15 23:36:48 -0400

    feature: add active storage and action text with trix editor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,6 @@ import Nabvar from "bootstrap"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,4 @@
 ActiveRecord::Base.transaction do
 end
 
-SeedMigration::Migrator.bootstrap(20210501215030)
+SeedMigration::Migrator.bootstrap(20210516013147)
