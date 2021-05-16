6a69715a0f993dfe300136a2d10debcb585ebcfc
Victoria Becerra <victoria.becerra@merlin.com>
2021-05-15 23:40:47 -0400

    refactor(article): remove content as text field

--- db/migrate/20210502002356_create_articles.rb
+++ db/migrate/20210502002356_create_articles.rb
@@ -3,7 +3,6 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.references :category, null: false, foreign_key: true
       t.string :title, null: false
-      t.text :content, null: false
       t.references :user, null: false, foreign_key: true
       t.boolean :active, default: :true
 
